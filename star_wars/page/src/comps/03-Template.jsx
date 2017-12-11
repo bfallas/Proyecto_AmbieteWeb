e9f251c9f6c2624d0dac1a0afd1676688bb1a095
diegocb1797 <34433753+diegocb1797@users.noreply.github.com>
2017-12-11 13:22:12 -0600

    Update 03-Template.jsx
    CSS tag

--- star_wars/page/src/comps/03-Template.jsx
+++ star_wars/page/src/comps/03-Template.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 export default class extends Component {
   render() {
 
-   const myStl = { width: "75%", color: "black", textAlign: "left", padding: "12px 20px", border: "2px solid black", margin: "6px auto", display: "block" , backgroundColor: "rgb(249, 241, 175)" }
+   
    return(
-     <div style={myStl} >
+     <div class="boxInfo">
        <div><strong>Name: </strong>{this.props.name}</div>
        <div><strong>URL: </strong>{this.props.loc}</div>
        <div><strong>More Info: </strong><Link to={"/" + this.props.loc}>LINK</Link></div>
